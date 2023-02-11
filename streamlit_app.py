76c3e23b320e05df10cee319318244faee789436
Tamali Kundu <77058229+tamali2807@users.noreply.github.com>
2023-02-11 14:58:35 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
-streamlit.title("My Parent's New Healthy Diner")
+streamlit.title("My Parent's New Healthy Dinner")
 
 streamlit.header('🍽️Breakfast Menu')
 streamlit.text('🥣Omega 3 & Blueberry Oatmeal')

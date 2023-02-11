e64b8c92b08ff8c9214367716b7fbb330336a93f
Tamali Kundu <77058229+tamali2807@users.noreply.github.com>
2023-02-11 14:56:42 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,5 @@ streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔🥚Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞Avacardo Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')

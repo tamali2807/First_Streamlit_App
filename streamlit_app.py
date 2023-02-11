9fd27ab4a0c9370d6476d6ac58ddb363d566bd8e
Tamali Kundu <77058229+tamali2807@users.noreply.github.com>
2023-02-11 14:57:51 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,10 @@ import streamlit
 
 streamlit.title("My Parent's New Healthy Diner")
 
-streamlit.header('Breakfast Menu')
+streamlit.header('🍽️Breakfast Menu')
 streamlit.text('🥣Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔🥚Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞Avacardo Toast')
 
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.header('🍌🥭🥝🍇 Build Your Own Fruit Smoothie')
